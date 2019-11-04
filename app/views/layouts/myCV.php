afb8a5064a25a7c9ad59aa7f23477647c155f566
nhuvnq-dsv <40037618+sarv19@users.noreply.github.com>
2019-11-05 01:14:29 +0700

    myCV

--- app/views/layouts/myCV.php
+++ app/views/layouts/myCV.php
@@ -1,1 +1,76 @@
-<h1>myCV</h1>
+<header class="cv-header">
+  <div class="page-wrapper d-flex ali-center">
+    <a href="#" class="d-b">
+      <img src="app/images/logo.jpg" alt="logo" class="logo-big" />
+    </a>
+
+    <nav class="category-menu d-flex ali-center">
+      <ul class="d-flex mr-5 pr-1">
+        <li>
+          <a href="#" class="txt--sub-heading">Templates</a>
+        </li>
+        <li>
+          <a href="#" class="txt--sub-heading">Examples</a>
+        </li>
+        <li>
+          <a href="#" class="txt--sub-heading">Guides</a>
+        </li>
+        <li>
+          <a href="#" class="txt--sub-heading">FAQ</a>
+        </li>
+      </ul>
+      <div class="toolkit">
+        <!-- <button class="btn-secondary txt--sub-heading mr-2">Sign up</button>
+        <button class="btn txt--sub-heading">Log in</button> -->
+        <span>Nhu Vo</span>
+        <img src="app/images/chevron-down-solid.svg" style="height: 12px;" />
+      </div>
+    </nav>
+  </div>
+</header>
+<div class="myCV" id="myCV">
+  <div class="myCV-body">
+    <div class="myCV-content">
+      <p class="myCV-title">Dashboard</p>
+
+      <div class="myCV-navbar">
+        <div class="myCV-nav">
+          <span class="myCV-nav-resume">Resume</span>
+          <span class="myCV-nav-CV">Cover Letter</span>
+        </div>
+        <button class="myCV-createNew">+ Create New</button>
+      </div>
+
+      <div class="myCV-displayCV">
+        <div>
+          <img src="app/images/resum-1.png" />
+          <div class="myCV-display-info">
+            <p class="myCV-info-title">Untitled</p>
+            <p class="myCV-info-date">Updated November, 01:05</p>
+            <div>
+              <p>Edit</p>
+              <p>Download PDF</p>
+              <p>Delete</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="app/images/resum-1.png" />
+          <div class="myCV-display-info">
+            <p class="myCV-info-title">Untitled</p>
+            <p class="myCV-info-date">Updated November, 01:05</p>
+            <div>
+              <p>Edit</p>
+              <p>Download PDF</p>
+              <p>Delete</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        < 1/2 >
+      </div>
+    </div>
+  </div>
+</div>
