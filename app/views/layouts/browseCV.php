22b9de946660f13a543f30c883c802f56bde092d
Nguyễn Tấn Dũng <tandung251298@gmail.com>
2019-11-06 12:44:09 +0700

    [FIX] browse-cv

--- app/views/layouts/browseCV.php
+++ app/views/layouts/browseCV.php
@@ -63,6 +63,26 @@
 
 <script>
  $(document).ready(function() {
+   var leftArrow = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="150.075" height="150.075" viewBox="0 0 60.075 60.075">
+        <defs>
+            <style>
+                .cls-1{fill:#ff6f61}
+            </style>
+        </defs>
+        <path id="arrow-alt-circle-left" d="M38.038 68.075a30.038 30.038 0 1 1 30.037-30.037 30.033 30.033 0 0 1-30.037 30.037zm14.05-35.367h-14.05v-8.587a1.455 1.455 0 0 0-2.483-1.03L21.711 37.008a1.44 1.44 0 0 0 0 2.047l13.844 13.917a1.453 1.453 0 0 0 2.483-1.03v-8.575h14.05a1.458 1.458 0 0 0 1.453-1.453v-7.752a1.458 1.458 0 0 0-1.453-1.454z" class="cls-1" transform="translate(-8 -8)"/>
+    </svg>
+    `
+    var rightArrow = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="150.075" height="150.075" viewBox="0 0 60.075 60.075">
+        <defs>
+            <style>
+                .cls-1{fill:#ff6f61}
+            </style>
+        </defs>
+        <path id="arrow-alt-circle-right" d="M38.038 8A30.038 30.038 0 1 1 8 38.038 30.033 30.033 0 0 1 38.038 8zm-14.05 35.367h14.05v8.587a1.455 1.455 0 0 0 2.483 1.03l13.844-13.917a1.44 1.44 0 0 0 0-2.047L40.521 23.092a1.453 1.453 0 0 0-2.483 1.03v8.587h-14.05a1.458 1.458 0 0 0-1.453 1.453v7.752a1.458 1.458 0 0 0 1.453 1.453z" class="cls-1" transform="translate(-8 -8)"/>
+    </svg>
+    `
    $('.slider').slick({
      centerMode: true,
      centerPadding: '0px',
@@ -70,8 +90,8 @@
      speed: 500,
      focusOnSelect:true,
      arrows: true,
-     prevArrow: '<img src="./app/assets/images/arrow-left.svg" class="slick-prev arrow arrow--prev">',
-     nextArrow: '<img src="./app/assets/images/arrow-right.svg" class="slick-next arrow">',
+     prevArrow: leftArrow,
+     nextArrow: rightArrow,
      responsive: [{
        breakpoint: 768,
        settings: {
