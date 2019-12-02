a7b86130433b9164a2cd8b1a0f75b1dcdb2152cd
tnmthu <tnmthu.maria98@gmail.com>
2019-12-02 10:05:13 +0700

    check isLogged at every route

--- app/router/routing.php
+++ app/router/routing.php
@@ -21,38 +21,75 @@ require_once __DIR__ . "/../controllers/TemplateController2.php";
 require_once __DIR__ . "/../controllers/Error404Controller.php";
 require_once __DIR__ . "/../controllers/FakeController.php";
 require_once __DIR__ . "/../services/authenticationService.php";
+require_once __DIR__ . "/../services/priviledgeService.php";
 
 
 define("ROOT_DIR", "web-assignment");
 
 Router::GET('/', function () {
-    $home = new LandingController();
-    $home->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $home = new HomeController();
+        $home->render();
+    } else {
+        $home = new LandingController();
+        $home->render();
+    }
 });
 
 Router::GET('/landing', function () {
-    $home = new HomeController();
-    $home->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $home = new HomeController();
+        $home->render();
+    } else {
+        $home = new LandingController();
+        $home->render();
+    }
 });
 
 Router::GET('/login', function () {
-    $login = new LoginController();
-    $login->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $home = new HomeController();
+        $home->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }
 });
 
 Router::GET('/register', function () {
-    $register = new RegisterController();
-    $register->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $home = new HomeController();
+        $home->render();
+    } else {
+        $register = new RegisterController();
+        $register->render();
+    }
 });
 
 Router::GET('/chooseCV', function () {
-    $chooseCV = new ChooseCVController();
-    $chooseCV->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $chooseCV = new ChooseCVController();
+        $chooseCV->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }
 });
 
 Router::GET('/myCV', function () {
-    $myCV = new MyCVController();
-    $myCV->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $myCV = new MyCVController();
+        $myCV->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }
 });
 
 Router::POST('/fake', function () {
@@ -61,18 +98,36 @@ Router::POST('/fake', function () {
 });
 
 Router::GET('/previewCV', function () {
-    $previewCV = new PreviewCVController();
-    $previewCV->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $previewCV = new PreviewCVController();
+        $previewCV->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }
 });
 
 Router::GET('/editCV', function () {
-    $editCV = new EditCVController();
-    $editCV->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $editCV = new EditCVController();
+        $editCV->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }    
 });
 
 Router::GET('/browseCV', function () {
-    $browseCV = new BrowseCVController();
-    $browseCV->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $browseCV = new BrowseCVController();
+        $browseCV->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }  
 });
 
 Router::GET('/testdb', function () {
@@ -81,8 +136,25 @@ Router::GET('/testdb', function () {
 });
 
 Router::GET('/template', function () {
-    $testdb = new TemplateController2();
-    $testdb->render();
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $testdb = new TemplateController();
+        $testdb->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }  
+});
+
+Router::GET('/template2', function () {
+    $priService = new PriviledgeService();
+    if ($priService->isLogin()) {
+        $testdb = new TemplateController2();
+        $testdb->render();
+    } else {
+        $login = new LoginController();
+        $login->render();
+    }
 });
 
 Router::POST('/login-authen', function() {
