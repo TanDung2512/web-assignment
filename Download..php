50a97fc12c08866062b1b4fe65f71be76875c7cd
TanDung2512 <36843017+TanDung2512@users.noreply.github.com>
2019-12-03 08:09:52 +0700

    Delete Download..php

--- Download..php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="refresh" content="2;url=http://www.graphviz.org/download/" />
-<title>Page Moved</title>
-</head>
-<body>
-This page has moved. Click <a href="http://www.graphviz.org/download/">here</a> to go to the new page.
-</body>
-</html>
